8d1aa3f303e76d7e059df97120ea39d7ed4eb95d
r1ch <rich@bradi.sh>
2019-08-07 14:45:38 +0100

    Update awsconfig.js

--- js/awsconfig.js
+++ js/awsconfig.js
@@ -4,6 +4,6 @@
 window.config = {
     roleArn: "arn:aws:iam::312837731527:role/GoogleAuthenticationRole",
     region: "eu-west-2",
-    apiGatewayUrl : "https://gxwvgsji6g.execute-api.eu-west-1.amazonaws.com",
+    apiGatewayUrl : "gxwvgsji6g.execute-api.eu-west-1.amazonaws.com",
     apiGatewayPath : "/live" 
 }
