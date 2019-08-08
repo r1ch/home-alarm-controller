e1635947ca3ba5979f7d284eff76d4d3a305d742
r1ch <rich@bradi.sh>
2019-08-08 09:21:57 +0100

    Update authentication.js

--- js/authentication.js
+++ js/authentication.js
@@ -6,7 +6,7 @@ function defer() {
 		res = resolve;
 		rej = reject;
 	});
-	promise.resolve = res;
+	promise.resolve = ()=>{console.log("Credentials Ready");res());
 	promise.reject = rej;
 	return promise;
 }
@@ -21,14 +21,8 @@ function authenticate(googleUser) {
 };
 
 function getIdToken(googleUser) {
-    // Useful data for your client-side scripts:
     profile = googleUser.getBasicProfile();
-    // Profile ID can be used in IAM roles for authorization by using accounts.google.com:sub
-    console.log("Google ID: " + profile.getId());
-    // The ID token needed for web identity authentication:
     var idToken = googleUser.getAuthResponse().id_token;
-    console.log("Google ID Token: " + idToken);
-    
     return new Promise(function (resolve) {
         resolve(idToken);
     });
@@ -46,8 +40,6 @@ function AWSSTSSignIn(idToken) {
             if (err) {
                 reject(err);
             } else {
-                // Returning STS response
-                console.log("STS credentials: " + JSON.stringify(data.Credentials));
                 resolve(data);
             }
         });
@@ -55,37 +47,33 @@ function AWSSTSSignIn(idToken) {
 }
 
 function handleSTSResponse(data) {
-    // Setting AWS config credentials globally
     AWS.config.credentials = new AWS.Credentials(
         data.Credentials.AccessKeyId,
         data.Credentials.SecretAccessKey,
         data.Credentials.SessionToken);
     AWS.config.region = window.config.region;
-    // Sending sign-in parameters to lambda function
-    var signInParameters = {
-        "sessionId": data.Credentials.AccessKeyId,
-        "sessionKey": data.Credentials.SecretAccessKey,
-        "sessionToken": data.Credentials.SessionToken
-    };
-    return new Promise(function (resolve) {
-        resolve(signInParameters);
-    })
+    return  CredentialsReady.resolve()
 }
 
 function signHttpRequest(method,path) {
-    // Setting AWS Signed header
-    var request = new AWS.HttpRequest(window.config.apiGatewayUrl, window.config.region);
-    request.method = method;
-    request.path = window.config.apiGatewayPath;
-    request.path += path
-    // Needed for proper signature generation
-    request.headers['Host'] = request.endpoint.host;
-    // Signing
-    var signer = new AWS.Signers.V4(request, 'execute-api');
-    signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
-    return new Promise(function (resolve) {
-        resolve(signer.request);
-    });
+    return CredentialsReady.then((method,path)=>{
+	    let request = new AWS.HttpRequest(window.config.apiGatewayUrl, window.config.region);
+	    request.method = method;
+	    request.path = window.config.apiGatewayPath;
+	    request.path += path
+	    request.headers['Host'] = request.endpoint.host;
+	    // Signing
+	    var signer = new AWS.Signers.V4(request, 'execute-api');
+	    signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+	    //repackage for Axios
+	    let r = signer.request
+	    delete r.headers.Host
+	    return {
+	    	method : r.method,
+		url : r.path,
+		baseURL : r.endpoint.href,
+		headers : r.headers
+	    }
 }
 
 function handleError(error) {
