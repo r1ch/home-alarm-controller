94d6742201b2646e1fa81d189ba059969e8b2bf6
r1ch <rich@bradi.sh>
2019-08-08 09:50:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-//
+Vue.component('google-login', {
+    template: '<div class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>'
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
