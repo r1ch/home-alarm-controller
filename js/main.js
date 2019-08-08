e59951bba4c1e82edd793050fc40f80ebc5f0a14
r1ch <rich@bradi.sh>
2019-08-08 16:23:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ Vue.component('time-line', {
     template: `
         <div class = "row">
             <div class="progress">
-                <div v-for="state in states" :class="progress-bar" :style="width:5%"></div>
+                <div v-for="state in states" :class="'progress-bar'" :style="'width:5%'"></div>
             </div>
         </div>
     `,
