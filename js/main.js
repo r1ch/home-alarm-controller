5db4ec8b7bfb22b0a9c7f64857eae3ef4c892501
r1ch <rich@bradi.sh>
2019-08-08 21:00:18 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,13 +65,429 @@ var app = new Vue({
         states: [],
     },
     mounted : function(){
-        signHttpRequest("GET","/alarm/monitor")
+        /*signHttpRequest("GET","/alarm/monitor")
         .then(axios)
         .then(({data:{shadow,metrics : {strategyState:strategies,alarmState:states,movement:movements}}})=>{
             this.shadow = shadow
             this.movements = movements
             this.states = states
-        })
+        })*/
+        {
+    this.shadow = {
+        "armed": false,
+        "strategy": "blind",
+        "state": "quiet"
+    }
+    this.strategies =  [
+            {
+                "detail": "blind",
+                "timestamp": "2019-08-08T18:37:00.000Z"
+            },
+            {
+                "detail": "standard",
+                "timestamp": "2019-08-08T17:37:00.000Z"
+            },
+            {
+                "detail": "blind",
+                "timestamp": "2019-08-08T16:27:00.000Z"
+            },
+            {
+                "detail": "standard",
+                "timestamp": "2019-08-08T07:27:00.000Z"
+            },
+            {
+                "detail": "blind",
+                "timestamp": "2019-08-08T06:59:00.000Z"
+            }
+        ]
+        this.states =  [
+            {
+                "detail": "quiet",
+                "timestamp": "2019-08-08T18:37:00.000Z"
+            },
+            {
+                "detail": "warning",
+                "timestamp": "2019-08-08T18:37:00.000Z"
+            },
+            {
+                "detail": "arming",
+                "timestamp": "2019-08-08T17:37:00.000Z"
+            },
+            {
+                "detail": "guarding",
+                "timestamp": "2019-08-08T17:37:00.000Z"
+            },
+            {
+                "detail": "quiet",
+                "timestamp": "2019-08-08T16:27:00.000Z"
+            },
+            {
+                "detail": "warning",
+                "timestamp": "2019-08-08T16:27:00.000Z"
+            },
+            {
+                "detail": "arming",
+                "timestamp": "2019-08-08T07:27:00.000Z"
+            },
+            {
+                "detail": "guarding",
+                "timestamp": "2019-08-08T07:27:00.000Z"
+            },
+            {
+                "detail": "quiet",
+                "timestamp": "2019-08-08T06:59:00.000Z"
+            }
+        ]
+        this.movement = [
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:56:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:56:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:55:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:55:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:54:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:53:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:53:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:52:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:44:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:44:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:39:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:38:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:38:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:37:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:36:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T19:36:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:35:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:34:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:33:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:31:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:30:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:29:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:27:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:26:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:25:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:21:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:20:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:15:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:14:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:05:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:04:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T19:03:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:55:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:51:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:48:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:47:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:46:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:46:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:45:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:45:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:44:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:44:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:43:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:43:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:42:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:42:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:41:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T18:39:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:39:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:38:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T18:37:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:25:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:24:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:24:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:23:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:21:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:20:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:19:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:18:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:17:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:16:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:15:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:14:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:13:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T17:12:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:12:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:11:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:10:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:09:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T17:03:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T16:59:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T16:58:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T16:29:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T16:29:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T16:28:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T16:27:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:15:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:14:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:13:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T07:12:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:12:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:11:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T07:11:00.000Z"
+            },
+            {
+                "detail": "Entry",
+                "timestamp": "2019-08-08T07:10:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T07:10:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T07:01:00.000Z"
+            },
+            {
+                "detail": "Lounge",
+                "timestamp": "2019-08-08T06:59:00.000Z"
+            }
+        ]
     }
 })  
 
