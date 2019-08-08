035d2f97525b8777a9e37bfa990e10e16c3b11e3
r1ch <rich@bradi.sh>
2019-08-08 10:07:35 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 Vue.component('google-login', {
-    template: '<div v-if = "CredentialsStatus.notReady" class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>'
+    template: '<div v-if = "Credentials" class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>'
 })
 
 var app = new Vue({
