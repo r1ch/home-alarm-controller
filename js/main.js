68c9d5e768bbef4696a515c775d402ce6ab9602b
r1ch <rich@bradi.sh>
2019-08-08 16:11:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,15 +7,14 @@ Vue.component('google-login', {
 })
 
 Vue.component('time-line', {
-    props:['movements'],
+    props:['movements','states'],
     template: `
-        <div id = "lineCont">
-            <div id = "line">
-                <div v-for="movement in processedMovements" class="circle" :id="'circle'+movement.index" :style="'left:'+movement.offset+'%;'">
-                    <i :class="'fas fa-'+movement.icon"></i>
-                </div>
-            </div>    
-    </div>`,
+        <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+        <div v-for="state in state" :class="'progress-bar bg-'{{state.color}}" role="progressbar" :style="'width: '{{state.offset}}%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    `,
     computed : {
         processedMovements(){
             if(this.movements.length >0){
@@ -31,7 +30,28 @@ Vue.component('time-line', {
                     icon : movement.detail == "Entry" ? "door-open" : "couch"
                 }))
             } else {
-                console.log("Evaluated too early")
+                console.log("No movements yet")
+            }
+        },
+        processedStates(){
+            if(this.states.length >0){
+                let earliest = new Date(this.states[this.states.length-1].timestamp)
+                let latest = new Date()
+                let span = latest-earliest
+                let offset  =  (time)=>(new Date(time)-earliest)*100/span
+                let progress = 0;
+                return this.states.map((state)=>{
+                    let slice = offset(state.timestamp)-progress
+                    let output = {
+                        date : new Date(state.timestamp),
+                        offset : slice,
+                        color : state.detail == "armed" ? "success" : "warning"
+                    }
+                    progress += slice
+                    return output
+                }))
+            } else {
+                console.log("No states yet")
             }
         }
     }
@@ -52,6 +72,7 @@ var app = new Vue({
         .then(({data:{shadow,metrics : {strategyState:strategies,alarmState:states,movement:movements}}})=>{
             this.shadow = shadow
             this.movements = movements
+            this.states = states
         })
     }
 })  
