1b259e316fe8c6210f604098d99dd9980f2cb3b7
r1ch <rich@bradi.sh>
2019-08-08 10:15:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 Vue.component('google-login', {
     template: '<div v-if = "!authenticated" class="g-signin2" data-width="200" data-height="50" data-onsuccess="authenticate" data-theme="dark"></div>',
     data : {
-        authenticated : false;
+        authenticated : false
     },
     mounted : function(){
-        Credentials.then(()=>{authenticated = true})
+        Credentials.then(()=>{this.authenticated = true})
     }
 })
 
