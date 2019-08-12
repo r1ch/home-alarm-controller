d227edf8daa8287d67c72113f86efd1acc551719
r1ch <rich@bradi.sh>
2019-08-12 13:08:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ Vue.component('time-line', {
                 <div v-for="strategy in processedStrategies" :class="'progress-bar bg-'+strategy.type" :style="'width:'+strategy.offset+'%'">{{strategy.detail}}</div>
             </div>
             <div class = "timelineEvent" v-for = "movement in processedMovements" v-if = "movement.show" :style = "'left:'+movement.offset+'%'">
-                <i :class="'fas fa-'+movement.icon"></i>
+                <i :class="'timelineMarker fas fa-'+movement.icon"></i>
             </div>
         </div>`,
     computed : {
