02edbd49ca510ea943be37c18411d511d785b592
r1ch <rich@bradi.sh>
2019-08-12 09:29:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ var app = new Vue({
         earliest(){
             const reducer = (a,b)=>({timestamp:Math.min(new Date(a.timestamp),new Date(b.timestamp))})
             let candidates = [...this.strategies, ...this.movements]
-            return !candidates ? new Date() : candidates.reduce(reducer)
+            return !candidates ? new Date() : new Date(candidates.reduce(reducer).timestamp)
         }
     },
     mounted : function (){
