8cca6be763786af40e174975a32cee77c4268990
r1ch <rich@bradi.sh>
2019-08-21 14:11:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,10 +59,10 @@ Vue.component('alarm-controls',{
 								<small>Nobody</small>
 							</td>
 							<td v-for = "person in presence">
-								<span = "fas fa-stack">
+								<span class = "fas fa-stack">
 									<i class = "fas fa-user fa-stack-2x"></i>
-									<i class = "fas fa-heart fa-stack-1x" style:"color:Tomato" 
-										v-if = "presence.indexOf('Guest') > 0 ? presence.length > 2 : presence.length > 1"
+									<i class = "fas fa-heart fa-stack-1x" style = "color:Tomato" 
+										v-if = "presence.indexOf('Guest') > 0 ? presence.length > 2 : presence.length > 1"   1"
 									></i>
 								</span>
 								<br>
