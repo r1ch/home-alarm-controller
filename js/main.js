20fccae217fd0e829c0975498d1780b25fa970c7
r1ch <rich@bradi.sh>
2019-08-22 11:52:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ Vue.component('time-d-three', {
 				.range([0, this.width])
 
 			let xAxis = d3.axisBottom(xScale)
-				.ticks(5); // specify the number of ticks
+				.ticks(6);
 
 			let yScale = d3.scalePoint()
 				.domain(this.movements.map(movement => iconMap[movement.detail]))
